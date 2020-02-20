4df7b7abb6d812b3165da8c9b72eacf11980b707
cmd08010 <colleendunion@gmail.com>
2020-02-19 19:38:03 -0500

    first

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const app = express();
-const db = require('./db');
-app.use(require('cors')());
-
-//don't forget your body parser!
-
-app.get('/api/departments', (req, res, next)=> {
+const express = require("express")
+const app = express()
+const db = require("./db")
+const path = require("path")
+const bodyParser = require("body-parser")
+let uuid = require("uuid/v4")
+app.use(require("cors")())
+app.use(bodyParser.json())
+app.get("/api/departments", (req, res, next) => {
   db.readDepartments()
-    .then( departments => res.send(departments)) 
-    .catch(next);
-});
-
-app.get('/api/users', (req, res, next)=> {
+    .then(departments => res.send(departments))
+    .catch(next)
+})
+app.get("/api/users", (req, res, next) => {
   db.readUsers()
-    .then( users => res.send(users)) 
-    .catch(next);
-});
-
-app.use((req, res, next)=> {
-  next({ status: 404, message: `page not found - ${ req.method } - ${req.url}` });
-});
-
-app.use((err, req, res, next)=> {
-  res.status(err.status || 500).send({ error: err.message ? err.message : err.toString()});
-});
-
-
-const port = process.env.PORT || 3000;
-
-db.sync()
-  .then(()=> {
-    console.log('synced');
-    app.listen(port, ()=> console.log(`listening on port ${port}`));
-  });
+    .then(users => res.send(users))
+    .catch(next)
+})
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `page not found - (method is ${req.method}) - (url is ${req.url})`
+  })
+})
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .send({ error: err.message ? err.message : err.toString() })
+})
+const port = process.env.PORT || 3000
+db.sync().then(() => {
+  console.log("synced")
+  app.listen(port, () => console.log(`listening on port ${port}`))
+})
