ae5bb390ea37d4a4c9fe5d8a8ff0b10a97e6ed07
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-02-22 09:48:03 -0500

    put still doesn't work

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const pg = require("pg")
-const { Client } = pg
-const uuid = require("uuid/v4")
-const client = new Client("postgres://localhost/departments")
-client.connect()
+const pg = require("pg");
+const { Client } = pg;
+const uuid = require("uuid/v4");
+const client = new pg.Client("postgres://localhost/departments");
+client.connect();
 const sync = async () => {
   // // DROP and RECREATE TABLES
   const SQL = `
@@ -11,74 +11,65 @@ const sync = async () => {
       DROP TABLE IF EXISTS departments;
       CREATE TABLE departments
       (
-        id UUID DEFAULT uuid_generate_v4(),
-        name VARCHAR NOT NULL,
-        CHECK (char_length(name) > 0),
-        PRIMARY KEY (id)
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        name VARCHAR NOT NULL unique,
+        CHECK (char_length(name) > 0)
       );
       CREATE TABLE users
       (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        name VARCHAR NOT NULL,
+        name VARCHAR NOT NULL unique,
         CHECK (char_length(name) > 0),
-        departmentID UUID,
-        FOREIGN KEY(departmentID) REFERENCES departments(id)
+        "departmentId" UUID references departments(id)
       );
-  `
-  await client.query(SQL)
+  `;
+  await client.query(SQL);
   // //remember "departmentId" will need to be in quotes
-}
+  const fashion = await createDepartment({ name: "Fashion" });
+  const paleontology = await createDepartment({ name: "Paleontology" });
+  await createUser({ name: "Rachel", departmentId: fashion.id });
+  await createUser({ name: "Ross", departmentId: paleontology.id });
+};
 
-//DEPARTMENTS
+const createDepartment = async ({ name }) => {
+  const SQL = `INSERT INTO departments (name) VALUES ($1) returning *;`;
+  const response = await client.query(SQL, [name]);
+  return response.rows[0];
+};
+const deleteDepartment = async id => {
+  const SQL = `DELETE FROM departments WHERE (id) = ($1);`;
+  await client.query(SQL, [id]);
+};
+const deleteUser = async id => {
+  const SQL = `DELETE FROM users WHERE (id) = ($1);`;
+  await client.query(SQL, [id]);
+};
+const createUser = async ({ name, departmentId }) => {
+  const SQL = `INSERT INTO users (name, "departmentId") VALUES ($1, $2) returning *;`;
+  const response = await client.query(SQL, [name, departmentId]);
+  return response.rows[0];
+};
+const updateUser = async ({ name, departmentId, id }) => {
+  const SQL = `UPDATE users SET (name) = ($1), "departmentId" = ($2) WHERE (id) = ($3) returning *;`;
+  const response = await client.query(SQL, [name, departmentId || null, id]);
+  return response.rows[0];
+};
+const updateDepartment = async ({ name, id }) => {
+  const SQL = `UPDATE departments SET (name) = ($1) WHERE (id) = ($2) returning *;`;
+  const response = await client.query(SQL, [name, id]);
+  return response.rows[0];
+};
 const readDepartments = async () => {
-  const SQL = `
-  SELECT * FROM departments
-  `
-  const response = await client.query(SQL)
-  return response.rows
-}
-
-const createDepartment = async name => {
-  const SQL = `
-
-
-  INSERT INTO departments (name) VALUES ($1)
-  returning *
-  `
-  const response = await client.query(SQL, [name])
-
-  return response.rows
-}
-
-//USERS
+  const SQL = `SELECT * FROM departments;`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
 const readUsers = async () => {
-  const SQL = `
-  SELECT * FROM users
-  `
-  const response = await client.query(SQL)
-  return response.rows
-}
+  const SQL = `SELECT * FROM users;`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
 
-const createUser = async (name, departmentId) => {
-  const SQL = `
-
-  INSERT INTO users (name, departmentID) VALUES ($1, $2)
-  returning *;
-  `
-  const response = await client.query(SQL, [name, departmentId])
-
-  console.log(response)
-  return response.rows
-}
-module.exports = {
-  sync,
-  readDepartments,
-  readUsers,
-  createDepartment,
-  createUser
-}
-//you will eventually need to export all of these
-/*
 module.exports = {
   sync,
   readDepartments,
@@ -87,7 +78,6 @@ module.exports = {
   createUser,
   deleteDepartment,
   deleteUser,
-  updateUser,
-  updateDepartment
+  updateDepartment,
+  updateUser
 };
-*/
--- server.js
+++ server.js
@@ -1,68 +1,93 @@
-const express = require("express")
-const app = express()
-const db = require("./db")
-const path = require("path")
-const bodyParser = require("body-parser")
-let uuid = require("uuid/v4")
-app.use(require("cors")())
-app.use(bodyParser.json())
+const express = require("express");
+const app = express();
+const db = require("./db");
+app.use(require("cors")());
+app.use(express.json());
+const path = require("path");
+const bodyParser = require("body-parser");
+let uuid = require("uuid/v4");
+app.use(bodyParser.json());
+const morgan = require("morgan");
 
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+//////////////////////////////////////get////////////////////////////////
 app.get("/api/departments", (req, res, next) => {
   db.readDepartments()
     .then(departments => res.send(departments))
-    .catch(next)
-})
-
+    .catch(next);
+});
 app.get("/api/departments/:id", (req, res, next) => {
   db.readDepartments()
     .then(departments => res.send(departments))
-    .catch(next)
-})
-
+    .catch(next);
+});
 app.get("/api/users", (req, res, next) => {
   db.readUsers()
     .then(users => res.send(users))
-    .catch(next)
-})
-
+    .catch(next);
+});
 app.get("/api/users/:id", (req, res, next) => {
   db.readUsers()
     .then(users => res.send(users))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.post("/api/departments", async (req, res, next) => {
-  //  console.log(req.body.name)
-  await db
-    .createDepartment(req.body.name)
-    .then(department => res.send(department[0]))
-    .catch(next)
-})
-app.post("/api/users", async (req, res, next) => {
-  console.log(req.body.departmentId)
-  await db
-    .createUser(req.body.name, req.body.departmentId)
+////////////////////////////////////post//////////////////////////////
+app.post("/api/departments", (req, res, next) => {
+  db.createDepartment(req.body)
+    .then(department => res.send(department))
+    .catch(next);
+});
+app.post("/api/users", (req, res, next) => {
+  db.createUser(req.body)
     .then(user => {
-      // console.log(user)
-      res.send(user)
+      res.send(user);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
+/////////////////////////////////////put//////////////////////////////
+app.put("/api/departments/:id", (req, res, next) => {
+  db.updateDepartment(req.body)
+    .then(dept => res.send(dept))
+    .catch(next);
+});
+app.put("/api/users/:id", (req, res, next) => {
+  db.updateUser({ ...req.body, id: req.params.id })
+    .then(user => res.send(user))
+    .catch(next);
+});
+
+/////////////////////////////////////delete//////////////////////////
+app.delete("/api/departments/:id", (req, res, next) => {
+  db.deleteDepartment(req.params.id)
+    .then(() => res.sendStatus(204)) //since no return
+    .catch(next);
+});
+app.delete("/api/users/:id", (req, res, next) => {
+  db.deleteUser(req.params.id)
+    .then(() => res.sendStatus(204)) //since no return
+    .catch(next);
+});
+
+/////////////////////////////////////use/////////////////////////////
 app.use((req, res, next) => {
   next({
     status: 404,
     message: `page not found - (method is ${req.method}) - (url is ${req.url})`
-  })
-})
+  });
+});
 app.use((err, req, res, next) => {
   res
     .status(err.status || 500)
-    .send({ error: err.message ? err.message : err.toString() })
-})
+    .send({ error: err.message ? err.message : err.toString() });
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 db.sync().then(() => {
-  console.log("synced")
-  app.listen(port, () => console.log(`listening on port ${port}`))
-})
+  console.log("synced");
+  app.listen(port, () => console.log(`listening on port ${port}`));
+});
