47312c64284ed368845546fbaa94cffd0d8a2cd3
David Brodie <david.brodie122@gmail.com>
2020-02-22 08:08:23 -0500

    Fix some small stuff and add a default department

--- db.js
+++ db.js
@@ -1,42 +1,43 @@
-const pg = require("pg")
-const { Client } = pg
-const uuid = require("uuid/v4")
-const client = new Client("postgres://localhost/departments")
-client.connect()
+const pg = require('pg');
+const { Client } = pg;
+const uuid = require('uuid/v4');
+const client = new Client('postgres://localhost/departments');
+client.connect();
+
 const sync = async () => {
   // // DROP and RECREATE TABLES
   const SQL = `
-      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-      DROP TABLE IF EXISTS users;
-      DROP TABLE IF EXISTS departments;
-      CREATE TABLE departments
-      (
-        id UUID DEFAULT uuid_generate_v4(),
-        name VARCHAR NOT NULL,
-        CHECK (char_length(name) > 0),
-        PRIMARY KEY (id)
-      );
-      CREATE TABLE users
-      (
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        name VARCHAR NOT NULL,
-        CHECK (char_length(name) > 0),
-        departmentID UUID,
-        FOREIGN KEY(departmentID) REFERENCES departments(id)
-      );
-  `
-  await client.query(SQL)
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS departments;
+    CREATE TABLE departments
+    (
+      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name VARCHAR NOT NULL,
+      CHECK (char_length(name) > 0)
+    );
+    CREATE TABLE users
+    (
+      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name VARCHAR NOT NULL,
+      CHECK (char_length(name) > 0),
+      "departmentId" UUID REFERENCES departments(id)
+  );
+
+    INSERT INTO departments (id, name) VALUES (uuid_generate_v4(), 'Engineering');
+  `;
+  await client.query(SQL);
   // //remember "departmentId" will need to be in quotes
-}
+};
 
 //DEPARTMENTS
 const readDepartments = async () => {
   const SQL = `
   SELECT * FROM departments
-  `
-  const response = await client.query(SQL)
-  return response.rows
-}
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
 
 const createDepartment = async name => {
   const SQL = `
@@ -44,39 +45,38 @@ const createDepartment = async name => {
 
   INSERT INTO departments (name) VALUES ($1)
   returning *
-  `
-  const response = await client.query(SQL, [name])
+  `;
+  const response = await client.query(SQL, [name]);
 
-  return response.rows
-}
+  return response.rows;
+};
 
 //USERS
 const readUsers = async () => {
   const SQL = `
   SELECT * FROM users
-  `
-  const response = await client.query(SQL)
-  return response.rows
-}
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
 
 const createUser = async (name, departmentId) => {
   const SQL = `
-
-  INSERT INTO users (name, departmentID) VALUES ($1, $2)
+  INSERT INTO users (name, "departmentId") VALUES ($1, $2)
   returning *;
-  `
-  const response = await client.query(SQL, [name, departmentId])
+  `;
+  const response = await client.query(SQL, [name, departmentId]);
 
-  console.log(response)
-  return response.rows
-}
+  console.log(response);
+  return response.rows;
+};
 module.exports = {
   sync,
   readDepartments,
   readUsers,
   createDepartment,
   createUser
-}
+};
 //you will eventually need to export all of these
 /*
 module.exports = {
--- server.js
+++ server.js
@@ -1,68 +1,70 @@
-const express = require("express")
-const app = express()
-const db = require("./db")
-const path = require("path")
-const bodyParser = require("body-parser")
-let uuid = require("uuid/v4")
-app.use(require("cors")())
-app.use(bodyParser.json())
+const express = require('express');
+const app = express();
+const db = require('./db');
+const path = require('path');
+const bodyParser = require('body-parser');
+let uuid = require('uuid/v4');
+app.use(require('cors')());
+app.use(bodyParser.json());
 
-app.get("/api/departments", (req, res, next) => {
+app.get('/api/departments', (req, res, next) => {
   db.readDepartments()
     .then(departments => res.send(departments))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.get("/api/departments/:id", (req, res, next) => {
+app.get('/api/departments/:id', (req, res, next) => {
   db.readDepartments()
     .then(departments => res.send(departments))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.get("/api/users", (req, res, next) => {
+app.get('/api/users', (req, res, next) => {
   db.readUsers()
     .then(users => res.send(users))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.get("/api/users/:id", (req, res, next) => {
+app.get('/api/users/:id', (req, res, next) => {
   db.readUsers()
     .then(users => res.send(users))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.post("/api/departments", async (req, res, next) => {
-  //  console.log(req.body.name)
+app.post('/api/departments', async (req, res, next) => {
   await db
     .createDepartment(req.body.name)
     .then(department => res.send(department[0]))
-    .catch(next)
-})
-app.post("/api/users", async (req, res, next) => {
-  console.log(req.body.departmentId)
+    .catch(next);
+});
+app.post('/api/users', async (req, res, next) => {
   await db
     .createUser(req.body.name, req.body.departmentId)
     .then(user => {
-      // console.log(user)
-      res.send(user)
+      res.send(user);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 app.use((req, res, next) => {
   next({
     status: 404,
     message: `page not found - (method is ${req.method}) - (url is ${req.url})`
-  })
-})
+  });
+});
 app.use((err, req, res, next) => {
   res
     .status(err.status || 500)
-    .send({ error: err.message ? err.message : err.toString() })
-})
+    .send({ error: err.message ? err.message : err.toString() });
+});
 
-const port = process.env.PORT || 3000
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const port = process.env.PORT || 3000;
 db.sync().then(() => {
-  console.log("synced")
-  app.listen(port, () => console.log(`listening on port ${port}`))
-})
+  console.log('synced');
+  app.listen(port, () => console.log(`listening on port ${port}`));
+});
